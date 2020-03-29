c28a38eeafaf602e5711db429e7600a335e8ca38
Patricia Centenera Béjar <patriciacentenerabejar@gmail.com>
2020-03-29 18:54:08 +0200

    Initial Commit

---
+++ Case2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Case2
+//
+//  Created by Belén on 29/03/2020.
+//  Copyright © 2020 IWEB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
